a016383864435272bc72aa3905cc1b53569709e9
burakuyanik55 <159728433+burakuyanik55@users.noreply.github.com>
2024-02-12 00:06:36 +0300

    .
    .

---
+++ src/main/java/com/testinium/test/automation/Bean.java
@@ -0,0 +1,11 @@
+package com.testinium.test.automation;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Bean {
+
+   public final static String BASE_URI="https://www.gratis.com/";
+   public static final WebDriver webDriver = new ChromeDriver();
+
+}
